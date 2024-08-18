e0fadf28e2b04b5760ed821933f673af9884c6bf
Chloe Smits <120358926+chloe-shecodes@users.noreply.github.com>
2024-08-18 15:51:18 +1000

    testExistingUser function added, without Login-link specification in error message

--- tests/existing-user.spec.js
+++ tests/existing-user.spec.js
@@ -6,5 +6,52 @@ let driver = new Builder()
   .setChromeOptions(new chrome.Options())
   .build();
 
-async function testExistingUser() {}
+async function testExistingUser() {
+  try {
+    await driver.get("https://app-moccona.letsweel.com/app/business-signup");
+
+    let emailField = await driver.findElement(
+      By.css('[data-testid="registration-email"]')
+    );
+    await emailField.sendKeys("existinguser@example.com");
+
+    let signUpButton = await driver.findElement(
+      By.css('[data-testid="submit-button"]')
+    );
+    await signUpButton.click();
+
+    let passwordField = await driver.wait(
+      until.elementLocated(By.css('[data-testid="registration-password"]')),
+      10000
+    );
+    await passwordField.sendKeys("ValidPass1!");
+
+    let termsCheckbox = await driver.findElement(
+      By.css('[data-testid="registration-terms"]')
+    );
+    await termsCheckbox.click();
+
+    let createAccountButton = await driver.findElement(
+      By.css('[data-testid="email-sign-up"]')
+    );
+    await createAccountButton.click();
+
+    let emailError = await driver.findElement(
+      By.css('[data-testid="registration-email-subtext-container"]')
+    );
+
+    let emailErrorText = await emailError.getText();
+    let expectedErrorText = "This account already exists. Login to continue";
+
+    if (emailErrorText === expectedErrorText) {
+      console.log("Test Passed: Correct error message displayed.");
+    } else {
+      console.log("Test Failed: Incorrect error message displayed.");
+    }
+  } catch (error) {
+    console.log("Test Failed: ", error);
+  } finally {
+    await driver.quit();
+  }
+}
 testExistingUser();
